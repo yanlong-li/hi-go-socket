9d4090e752c9c135cdc8c8be2bfaee6dbf44a282
Yanlong-li <ahlyl94@gmail.com>
2020-05-09 14:22:54 +0800

    增加支持多包注册

--- io/network/packet/base.go
+++ io/network/packet/base.go
@@ -10,10 +10,17 @@ var Packets = make(map[uint32]interface{})
 // 反注册包处理动作编码
 var RPackets = make(map[interface{}]uint32)
 
-// 包服务注册应由引用源程序处理并注册到当前包的packets之下
-func Register(op uint32, packet interface{}) {
+// 包服务注册应由引用源程序处理并注册到当前包的packets之下 支持多包注册，op自 ++
+func Register(op uint32, packet interface{}, packets ...interface{}) {
 	Packets[op] = packet
 	RPackets[reflect.TypeOf(packet)] = op
+
+	// 多包注册，op自 ++
+	for _, v := range packets {
+		op++
+		Packets[op] = v
+		RPackets[reflect.TypeOf(packet)] = op
+	}
 }
 
 func OpCode(PacketModel interface{}) uint32 {
